ce95b3d77594a02ac0cb92d7ee83c92dc7bef689
Qleksa <qleksa@gmail.com>
2018-04-16 22:17:31 +0200

    upload

---
+++ c/vezba3/zad3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int A[30], V, i, s = 0, n = 0;
+
+    printf("V = ");
+
+    scanf("%d", &V);
+
+    // unos
+    while(s <= V) {
+        scanf("%d", &A[n]); 
+        s += A[n]; 
+        n++;
+    }
+
+    // stampa
+    printf("Niz: ");
+    for (i = 0; i < n; i++) printf("%d ", A[i]);
+
+    // broj elemenata i srednja vrednost
+    printf("\nBroj elemenata: %d\n", n+1);
+    printf("Srednja vrednost: %d", s/(n+1));
+}
